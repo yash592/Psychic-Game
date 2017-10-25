41f592f16aba50ddc409ff8b6aa40dd5adff216a
Yash <yash592@gmail.com>
2017-10-24 22:57:27 -0700

    Second commit with logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,48 @@
-alert("connected!");
+// define win, loss and guesses
+
+// 	Define an array of alphabets
+
+// 	Then ask computer to pick a randon alphabet
+
+// 	Listen to what user types thru onkey
+
+// 	compare userguess to computerguess
+
+// 	if same then wins++ or else losses++
+
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+
+document.onkeyup = function(event) {
+
+var userguess = event.key;
+console.log(userguess);
+
+var computerguess = alphabets[Math.floor(Math.random()*alphabets.length)];
+console.log(computerguess);
+
+if (userguess === computerguess){
+   wins++;
+   alert("You won brah!");
+}
+
+
+else {
+	
+	losses++;
+} 
+
+	
+
+var winscore = 	document.getElementById("win");
+winscore.textContent = "Wins: " + wins;
+
+var losescore = document.getElementById("loss");
+losescore.textContent = "Losses: " + losses;
+
+var guesssofar = document.getElementById("guessfar");
+guesssofar.textContent = "Guesses so far: " + event.key;
+
+}
